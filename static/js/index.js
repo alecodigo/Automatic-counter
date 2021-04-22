be341a280b2cf8412424b38ec7352ea144148709
Alejandro <alejandroingelec19@gmail.com>
2021-04-22 08:53:56 -0400

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,33 @@
+window.onload = init()
+
+function init() {
+    document.querySelector('.start').addEventListener('click', Start)
+    document.querySelector('.stop').addEventListener('click', Stop)
+    document.querySelector('.reset').addEventListener('click', Reset)
+    counter = 0
+    id = 0
+}
+
+function Start() {
+        draw()
+        id = setInterval(draw,1000);
+        document.querySelector('.start').removeEventListener("click", Start);
+}
+
+function draw() {
+    counter++
+    const display = document.querySelector('#value')
+    display.textContent = counter
+}
+
+function Stop() {
+    clearInterval(id);
+    const btnStart = document.querySelector('.start').addEventListener('click', Start)
+}
+
+function Reset() {
+    counter = 0
+    clearInterval(id);
+    const display = document.querySelector('#value')
+    display.textContent = counter
+}
